6d922932d5471b47affad4283e44560824913da1
rawkasa <rawkasa@naver.com>
2025-05-20 22:47:49 +0900

    next() 메서드는 제너레이터의 값을 꺼낼 수 있으며, 그 과정에서 JSON 형태로 추출 가능함. 이를 연속적으로 호출하면 value가 순차적으로 나오며, 세번째에서 {done: true}가 나옴.

--- Chapter4/4-31.js
+++ Chapter4/4-31.js
@@ -9,3 +9,11 @@ let generator = generateSequence();
 let one = generator.next();
 
 alert(JSON.stringify(one)); // {value: 1, done: false}
+
+let two = generator.next();
+
+alert(JSON.stringify(two)); // {value: 2, done: false}
+
+let three = generator.next();
+
+alert(JSON.stringify(three)); // {value: 3, done: true}
