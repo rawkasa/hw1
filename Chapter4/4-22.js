d73d717e18d992ed7ff7ec3fc0ef8e806a53481a
rawkasa <rawkasa@naver.com>
2025-05-20 17:27:48 +0900

    Promise를 이용하면 함수 실행에 있어 성공(resolve)과 실패(reject)의 경우를 분리하여 처리할 수 있음. 이때, 함수 호출부에서는 .then과 .catch로 처리할 수 있음.

---
+++ Chapter4/4-22.js
@@ -0,0 +1,12 @@
+function devide(numA, numB) {
+    return new Promise((resolve, reject) => {
+        if (numB === 0) reject(new Error("Unable to devide by 0."))
+        else resolve(numA / numB)
+    })
+}
+
+devide(8, 2)
+    .then((result) => console.log("성공:", result))
+    .catch((error) => console.log("실패:", error))
+
+// 결과 == 성공: 4
