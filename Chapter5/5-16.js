09e290b56e943acd5669d1178a0a3c3bef10a1da
rawkasa <rawkasa@naver.com>
2025-06-05 21:29:34 +0900

    디바운스(Debounce) 기법을 사용하면 특정 이벤트가 연속해서 발생할 때, 마지막 이벤트 발생 후 일정 시간(wait)이 지나야 실제 함수를 한 번만 호출하도록 지연시킬 수 있으며, 이를 사용하면 불필요한 반복 호출을 막을 수 있음.

--- Chapter5/5-16.js
+++ Chapter5/5-16.js
@@ -1,9 +1,17 @@
 var debounce = function (eventName, func, wait) {
     var timeoutId = null;
     return function (event) {
-    console.log(eventName, 'event 발생');
         var self = this;
+        console.log(eventName, 'event 발생');
         clearTimeout(timeoutId)
         timeoutId = setTimeout(func.bind(self, event), wait);
-    }
+    };
 };
+
+var moveHandler = function (e) {
+    console.log('move 이벤트 처리');
+};
+var wheelHandler = function (e) {
+    console.log('wheel 이벤트 처리');
+};
+document.body.addEventListener('mousemove', debounce('move', moveHandler, 500));document.body.addEventListener('mousewheel', debounce('wheel', wheelHandler, 500));
