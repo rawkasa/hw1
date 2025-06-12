e635ae4144fe569a2d6095eed895a47db246de5f
rawkasa <rawkasa@naver.com>
2025-06-12 19:58:40 +0900

    Person 생성자 함수는 인자로 받은 name을 객체의 속성 _name에 저장하며, Prototype 메서드는 객체를 여러 개 생성해도 메모리에 한번만 저장함. 이때, Suzi.__proto__는 Person.prototype을 직접 참조하며, getName() 메서드가 호출되었을 때 this는 Suzi가 아니라 Suzi.__proto__(Person.prototype)을 가리키며, 이 때 _name 속성은 Prototype이 아닌 객체 Suzi에만 존재하므로 undefined를 반환함. 이를 위해서는 Prototype 객체에 _name 속성을 직접 추가하면 정상적인 결과가 나타나나, 이는 비정상적인 방법임. 객체지향 스타일을 사용할 경우, getName()의 this가 Suzi, iu 인스턴스를 가리키는 경우 정상적으로 _name에 접근이 가능함.

--- Chapter6/6-01.js
+++ Chapter6/6-01.js
@@ -4,5 +4,15 @@ var Person = function (name) {
 Person.prototype.getName = function() {
     return this._name;
 };
+
 var Suzi = new Person('Suzi');
 Suzi.__proto__.getName();       // Undefined
+
+var Suzi = new Person('Suzi');
+Suzi.__proto__._name = 'SUZI__proto__';
+Suzi.__proto__.getName();       // SUZI__proto__
+
+var Suzi = new Person('Suzi', 20);
+Suzi.getName();            // Suzi
+var iu = new Person('Jieun', 20);
+iu.getName();            // Jieun
